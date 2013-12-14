e10b13f3d68a8217cfb7b4a91612297407fcc44a
Raphael Catolino <raphael.catolino@gmail.com>
2013-12-14 21:21:11 +0100

    Remove the awkward enums representing the option types.

--- lib.rs
+++ lib.rs
@@ -1,5 +1,5 @@
-#[desc = "Library to parse simple command line arguments"]
-#[license = "MIT"]
+#[desc = "Library to parse simple command line arguments"];
+#[license = "MIT"];
 
 // argument : string passed by the user via the command line
 // command : kind of argument that is unique, doesn't start with '-',
@@ -8,35 +8,30 @@
 //         Can only be last or followed by other values.
 use std::hashmap::HashMap;
 use std::result::Result;
+use Flags::OptFlags;
 
-pub enum OptValue {
-  StrValue(&'static str),
-  IntValue(int),
-  BoolValue(bool),
-  Present,
-  Empty
-}
-
-pub enum OptValueType {
-  Str,
-  Int,
-  Bool,
-  NoValue
+pub mod Flags {
+  pub enum OptFlags {
+    Defaults = 0,
+    AcceptMultiple = 1 << 0,
+    Hidden = 1 << 1,
+    TakesArg = 1 << 2,
+    TakesOptionalArg = 1 << 3
+  }
 }
 
 pub struct Opt {
   long_name: Option<&'static str>,
   short_name: Option<&'static str>,
   description: Option<&'static str>,
-  value_type: OptValueType,
-  value: OptValue
+  value: Option<~FromStr>,
+  flags: OptFlags
 }
 
 pub struct Cmd {
   name: &'static str,
   description: Option<&'static str>,
-  options: ~[~Opt],
-  value: OptValue,
+  options: ~[~Opt]
 }
 
 pub struct OptContext {
@@ -50,49 +45,27 @@ pub struct OptContext {
   raw_args: ~HashMap<~str, ~RawArg>
 }
 
-impl Opt {
-  fn is_named(&self) -> bool {
-    if self.long_name.is_none() && self.short_name.is_none() {
-      return false;
-    }
-    return true;
-  }
-
-  fn is_valid(&self) -> Result<(), &'static str> {
-    if !self.is_named() {
-      match self.value_type {
-        NoValue => return Err("Anonymous arguments always have a value"),
-        _ => {}
-      }
-    }
-
-    match self.value {
-      // Check if the default value for the option matches the
-      // option value type.
-      StrValue(_) => match self.value_type {
-        Str => {}, _ => return Err("Unexpected string as default value in option.")
-      },
-      IntValue(_) => match self.value_type {
-        Int => {}, _ => return Err("Unexpected int as default value in option.")
-      },
-      BoolValue(_) => match self.value_type {
-        Bool => {}, _ => return Err("Unexpected boolean as default value in option.")
-      },
-      Present => match self.value_type {
-        _ => return Err("Unexpected 'Present' as default value in option.")
-      },
-      Empty => {}
-    }
-
-    return Ok(());
-  }
-}
-
 priv struct RawArg {
   checked: bool,      // Once a call to check_option has matched a RawArg.
   option: bool,       // Options start with - or --
   position: ~[uint],      // Postion in the user provided arg list. Starts at 0.
                       // The program name is ignored.
+}
+
+impl Opt {
+  fn new(long: Option<&'static str>,
+         short: Option<&'static str>,
+         descr: Option<&'static str>,
+         flags: OptFlags) -> Result<~Opt, &'static str> {
+    if long.is_none() && short.is_none() {
+      Err("An option needs either a short or a long name")
+    } else {
+      Ok(~Opt {
+        long_name: long, short_name: short, description: descr,
+        value: None, flags: flags
+      })
+    }
+  }
 }
 
 impl RawArg {
@@ -148,26 +121,18 @@ impl OptContext {
                            long: Option<&'static str>,
                            short: Option<&'static str>,
                            descr: Option<&'static str>,
-                           val_type: OptValueType,
-                           default_val: OptValue) ->
+                           flags: OptFlags) ->
     Result<&'a mut OptContext, &'static str> {
 
-    self.add_option(~Opt { long_name: long,
-                           short_name: short,
-                           description: descr,
-                           value_type: val_type,
-                           value: default_val })
+    match Opt::new(long, short, descr, flags) {
+      Ok(opt) => Ok(self.add_option(opt)),
+      Err(msg) => Err(msg),
+    }
   }
 
-  pub fn add_option<'a>(&'a mut self, opt: ~Opt) ->
-    Result<&'a mut OptContext, &'static str> {
-    match opt.is_valid() {
-      Err(msg) => return Err(msg),
-      _ => {}
-    }
-
+  pub fn add_option<'a>(&'a mut self, opt: ~Opt) -> &'a mut OptContext {
     self.options.push(opt);
-    Ok(self)
+    self
   }
 
   pub fn print_help(&self) {
--- test.rs
+++ test.rs
@@ -1,95 +1,25 @@
 extern mod cmdparse;
 
-use cmdparse::*;
+use cmdparse::OptContext;
+use cmdparse::Flags;
 
 #[test]
-fn test_create_option1() {
+fn test_create_option_valid() {
   let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
   // Those are valid options with no value:
-  ctx.create_option(Some("long"), Some("l"), Some("description"),
-                    NoValue, Empty).unwrap();
-  ctx.create_option(Some("long2"), None, Some("description"),
-                    NoValue, Empty).unwrap();
-  ctx.create_option(None, Some("r"), Some("description"),
-                    NoValue, Empty).unwrap();
-  ctx.create_option(Some("long3"), Some("l"), None,
-                    NoValue, Empty).unwrap();
+  ctx.create_option(Some("long"), Some("l"), Some("description"), Flags::Defaults).unwrap();
+  ctx.create_option(Some("long2"), None, Some("description"), Flags::Defaults).unwrap();
+  ctx.create_option(Some("long3"), Some("l"), None, Flags::Defaults).unwrap();
+  ctx.create_option(Some("long3"), None, None, Flags::Defaults).unwrap();
+  ctx.create_option(None, Some("r"), Some("description"), Flags::Defaults).unwrap();
+  ctx.create_option(None, Some("a"), None, Flags::Defaults).unwrap();
 }
 
 #[test]
-fn test_create_option2() {
+fn test_create_option_invalid() {
   let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  // Those are valid options which take values but have no defaults
-  ctx.create_option(None, Some("a"), None, Str, Empty).unwrap();
-  ctx.create_option(None, Some("b"), None, Int, Empty).unwrap();
-  ctx.create_option(None, Some("c"), None, Bool, Empty).unwrap();
-}
-
-#[test]
-fn test_create_option3() {
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  // Those are valid options which take values and have default values.
-  ctx.create_option(None, Some("d"), None, Str, StrValue("test")).unwrap();
-  ctx.create_option(None, Some("e"), None, Int, IntValue(1)).unwrap();
-  ctx.create_option(None, Some("f"), None, Bool, BoolValue(true)).unwrap();
-}
-
-#[test]
-fn test_create_option4() {
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  // Those are invalid options with no values.
-  ctx.create_option(None, Some("g"), None, NoValue, StrValue("test")).unwrap_err();
-  ctx.create_option(None, Some("h"), None, NoValue, IntValue(1)).unwrap_err();
-  ctx.create_option(None, Some("i"), None, NoValue, BoolValue(true)).unwrap_err();
-  ctx.create_option(None, Some("j"), None, NoValue, Present).unwrap_err();
-}
-
-#[test]
-fn test_create_option5() {
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  // Those are invalid options with str values.
-  ctx.create_option(None, Some("k"), None, Str, Present).unwrap_err();
-  ctx.create_option(None, Some("l"), None, Str, IntValue(1)).unwrap_err();
-  ctx.create_option(None, Some("m"), None, Str, BoolValue(true)).unwrap_err();
-}
-
-#[test]
-fn test_create_option6() {
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  // Those are invalid options with int values.
-  ctx.create_option(None, Some("n"), None, Int, Present).unwrap_err();
-  ctx.create_option(None, Some("o"), None, Int, StrValue("test")).unwrap_err();
-  ctx.create_option(None, Some("p"), None, Int, BoolValue(true)).unwrap_err();
-}
-
-#[test]
-fn test_create_option7() {
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  // Those are invalid options with bool values.
-  ctx.create_option(None, Some("q"), None, Bool, Present).unwrap_err();
-  ctx.create_option(None, Some("r"), None, Bool, IntValue(1)).unwrap_err();
-  ctx.create_option(None, Some("s"), None, Bool, StrValue("test")).unwrap_err();
-}
-
-#[test]
-fn test_create_option8() {
-  // Can't have an argument with no value.
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  ctx.create_option(None, None, None, NoValue, Empty).unwrap_err();
-}
-
-#[test]
-fn test_parse_valid() {
-  let mut ctx = OptContext::new("test [option] [argument]", ~[~"test"]);
-  ctx.create_option(Some("long1"), Some("s"), Some("description"),
-                    NoValue, Empty);
-  ctx.create_option(Some("long2"), None, Some("description"),
-                    NoValue, Empty);
-  ctx.create_option(None, Some("r"), Some("description"),
-                    NoValue, Empty);
-  ctx.create_option(Some("help"), Some("h"), None,
-                    NoValue, Empty);
-  ctx.create_option(None, None, None, NoValue, Empty);
-
+  // Those are valid options which take values but have no Flags::Defaults
+  ctx.create_option(None, None, None, Flags::Defaults).unwrap_err();
+  ctx.create_option(None, None, Some("description"), Flags::Defaults).unwrap_err();
 }
 
